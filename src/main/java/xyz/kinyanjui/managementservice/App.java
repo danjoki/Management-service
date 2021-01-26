47060380a3df0d92d7219797480d0542ef53c668
Daniel Njoroge Kinyanjui <dkinyanjui@safaricom.co.ke>
2021-01-26 18:43:18 +0300

    Backend> Links

--- src/main/java/xyz/kinyanjui/managementservice/App.java
+++ src/main/java/xyz/kinyanjui/managementservice/App.java
@@ -1,6 +1,11 @@
 package xyz.kinyanjui.managementservice;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 import xyz.kinyanjui.managementservice.configs.GlobalVariables;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -15,5 +20,24 @@ public class App {
     public static void main(String[] args){
         port(getHerokuAssignedPort());
         staticFileLocation(GlobalVariables.PUBLIC);
+
+        get(GlobalVariables.HOME_PATH, ((request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, GlobalVariables.INDEX_HBS);
+        }));
+
+        get(GlobalVariables.EMPLOYEE_PATH, ((request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, GlobalVariables.EMPLOYEE_HBS);
+        }));
+
+        get(GlobalVariables.DEPARTMENT_PATH, ((request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return render(model, GlobalVariables.DEPARTMENT_HBS);
+        }));
+    }
+
+    public static String render(Map<String, Object> model, String path){
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, path));
     }
 }
--- src/main/java/xyz/kinyanjui/managementservice/configs/GlobalVariables.java
+++ src/main/java/xyz/kinyanjui/managementservice/configs/GlobalVariables.java
@@ -3,4 +3,14 @@ package xyz.kinyanjui.managementservice.configs;
 public class GlobalVariables {
     public static final String PUBLIC = "/public";
     public static final String PORT = "PORT";
+
+    //Path
+    public static final String HOME_PATH = "/";
+    public static final String EMPLOYEE_PATH = "/employee";
+    public static final String DEPARTMENT_PATH = "/departments";
+
+    //HBS
+    public static final String INDEX_HBS = "index.hbs";
+    public static final String EMPLOYEE_HBS = "staff.hbs";
+    public static final String DEPARTMENT_HBS = "departments.hbs";
 }
